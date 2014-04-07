dc39f7c39eec3f5ef7c01a6f899050daddbab303
Chris Mitchell <cmitchell@doubleencore.com>
2014-04-07 15:11:40 -0600

    Add comment to empty fragment

--- app/src/main/java/com/cmitchell687/books/app/fragments/BookListFragment.java
+++ app/src/main/java/com/cmitchell687/books/app/fragments/BookListFragment.java
@@ -2,5 +2,8 @@ package com.cmitchell687.books.app.fragments;
 
 import android.app.ListFragment;
 
+/**
+ * Extend List Fragment to benefit from loading, and empty views.
+ */
 public class BookListFragment extends ListFragment {
 }
